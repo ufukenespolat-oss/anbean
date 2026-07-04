6e59e4b0962dbdd1228d07143fe016aef437e194
anbean <ufukenespolat@gmail.com>
2026-07-04 04:39:16 +0300

    Update script.js
    Removed click event listeners from card elements and updated price and weather information.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 console.log("ANBEAN hazır!");
 
-document.querySelectorAll(".card").forEach(card=>{
-    card.addEventListener("click",()=>{
-        alert(card.querySelector("h2").innerText);
-    });
-});
+document.getElementById("gold-price").innerText="4.352 ₺";
+document.getElementById("usd-price").innerText="41.12 ₺";
+document.getElementById("eur-price").innerText="48.25 ₺";
+document.getElementById("weather").innerText="☀️ 31°C";
