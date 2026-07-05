e96f8769b40f36d8749abd86fa5efb4f3bec9272
anbean <ufukenespolat@gmail.com>
2026-07-05 04:40:50 +0300

    Change API to fetch rates and save to JSON
    Updated API endpoint to fetch exchange rates and gold prices. Saved the output to a JSON file.

--- update.py
+++ update.py
@@ -1,16 +1,22 @@
 import requests
+import json
 
-url = "https://api.genelpara.com/json/?list=doviz&sembol=USD,EUR"
+API_KEY = "751d9f00a2ef59d9a40b2edb5f3b1995"
 
-r = requests.get(
-    url,
-    headers={
-        "User-Agent": "Mozilla/5.0",
-        "Accept": "application/json"
-    },
-    timeout=20
-)
+url = f"https://api.metalpriceapi.com/v1/latest?api_key={API_KEY}&base=TRY&currencies=USD,EUR,XAU"
 
-print("STATUS:", r.status_code)
-print("TEXT:")
-print(r.text)
+r = requests.get(url, timeout=20)
+data = r.json()
+
+rates = data["rates"]
+
+output = {
+    "usd": round(1 / rates["USD"], 2),
+    "eur": round(1 / rates["EUR"], 2),
+    "gold": round(31.1035 / rates["XAU"], 2)
+}
+
+with open("data.json", "w", encoding="utf-8") as f:
+    json.dump(output, f, ensure_ascii=False, indent=2)
+
+print("Güncellendi.")
